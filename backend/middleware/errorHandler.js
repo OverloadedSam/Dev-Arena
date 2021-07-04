ba98d803073647cc03f8eab891aa4df48ceddf39
Samdish <samdeeshsingh001@gmail.com>
2021-07-04 21:26:45 +0530

    Fix the bug where custom error message was not used for sending response to the client

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -2,6 +2,7 @@ const ErrorResponse = require("../utils/errorResponse");
 
 const errorHandler = (err, req, res, next) => {
     let error = { ...err };
+    error.message = err.message;
 
     // Mongoose error for duplicate field value.
     if (error.code === 11000) {
