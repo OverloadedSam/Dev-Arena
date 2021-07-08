713155b976244a9b1fee2ce679239253e240a2b1
Samdish <samdeeshsingh001@gmail.com>
2021-07-08 20:07:53 +0530

    Add route for geeting all posts and implement its controller function

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -2,7 +2,7 @@ const Post = require("../models/post");
 const asyncHandler = require("../middleware/asyncHandler");
 const ErrorResponse = require("../utils/errorResponse");
 
-// @route    GET /api/post:post_id
+// @route    GET /api/post/:post_id
 // @desc     Get post by specifying id.
 // @access   Protected
 const getPostById = asyncHandler(async (req, res, next) => {
@@ -18,6 +18,22 @@ const getPostById = asyncHandler(async (req, res, next) => {
     });
 });
 
+// @route    GET /api/posts
+// @desc     Get all posts (latest first).
+// @access   Protected
+const getPosts = asyncHandler(async (req, res, next) => {
+    const posts = await Post.find()
+        .populate("user", ["name"])
+        .sort({ createdAt: -1 });
+
+    return res.status(200).json({
+        success: true,
+        status: 200,
+        data: posts,
+    });
+});
+
 module.exports = {
     getPostById,
+    getPosts,
 };
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const protect = require("../middleware/protect");
 const checkObjectId = require("../middleware/checkObjectId");
-const { getPostById } = require("../controllers/posts");
+const { getPostById, getPosts } = require("../controllers/posts");
 
 router.get("/post/:post_id", checkObjectId("post_id"), protect, getPostById);
+router.route("/posts").get(protect, getPosts);
 
 module.exports = router;
