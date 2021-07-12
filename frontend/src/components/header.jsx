21c45f948186da05bf4f4d0f0f2d3cc1336f4677
Samdish <samdeeshsingh001@gmail.com>
2021-07-12 21:59:41 +0530

    Fixe design issue by improving the header component

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -8,36 +8,45 @@ class Header extends Component {
     render() {
         return (
             <div className="header-wrapper">
-                <Navbar className="py-2 header" bg="primary" expand="lg">
+                <Navbar
+                    className="py-2 header navbar-static-top"
+                    bg="primary"
+                    expand="lg"
+                >
                     <Navbar.Brand className="text-light">
                         <h4 className="mb-0 text-warning">DEV ARENA</h4>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Navbar.Collapse
+                        id="basic-navbar-nav"
+                        className="justify-content-between"
+                    >
                         <Nav className="mr-auto light">
                             <NavLink
                                 to="/home"
-                                className="nav-link ml-3 text-decoration-none"
+                                className="nav-link mx-auto navbar-item text-decoration-none"
                             >
-                                <i className="fa fa-home"></i> Home
+                                <i className="ml-3 px-0 fa fa-home"></i> Home
                             </NavLink>
                             <NavLink
                                 to="/developers"
-                                className="nav-link ml-3 text-decoration-none"
+                                className="nav-link mx-auto navbar-item text-decoration-none"
                             >
-                                <i className="fa fa-users"></i> Developers
+                                <i className="ml-3 px-0 fa fa-users"></i>{" "}
+                                Developers
                             </NavLink>
                             <NavLink
                                 to="/posts"
-                                className="nav-link ml-3 text-decoration-none"
+                                className="nav-link mx-auto navbar-item text-decoration-none"
                             >
-                                <i className="fa fa-newspaper-o"></i> Posts
+                                <i className="ml-3 px-0 fa fa-newspaper-o"></i>{" "}
+                                Posts
                             </NavLink>
                         </Nav>
                         <Nav className="ml-auto">
                             <NavLink
                                 to="/login"
-                                className="nav-link text-decoration-none"
+                                className="nav-link mx-auto navbar-item text-decoration-none"
                             >
                                 <Button variant="danger">
                                     <i className="fa fa-sign-in"> </i> Log in
@@ -45,7 +54,7 @@ class Header extends Component {
                             </NavLink>
                             <NavLink
                                 to="/register"
-                                className="nav-link text-decoration-none"
+                                className="nav-link mx-auto navbar-item text-decoration-none"
                             >
                                 <Button variant="outline-success">
                                     <i className="fa fa-user-plus"> </i>{" "}
@@ -54,7 +63,7 @@ class Header extends Component {
                             </NavLink>
                             {/* <NavLink
                                 to="/logout"
-                                className="nav-link text-decoration-none"
+                                className="nav-link mx-auto navbar-item text-decoration-none"
                             >
                                 <Button variant="warning">
                                     <i className="fa fa-power-off"> </i> Logout
