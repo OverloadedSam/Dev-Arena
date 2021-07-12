1e8ab78e6176e78f9eec40732260f26a101975cd
Samdish <samdeeshsingh001@gmail.com>
2021-07-12 09:11:38 +0530

    Implement routing for the app and add routing for homscreen and other component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,33 @@
 import React from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    Switch,
+} from "react-router-dom";
+import Header from "./components/header";
+import HomeScreen from "./screens/homeScreen";
+
+import "./css/bootstrap.min.css";
+import "./css/index.css";
+import "./css/app.css";
 
 const App = () => {
-    return <div className="App"></div>;
+    return (
+        <>
+            <Router>
+                <>
+                    <Header />
+                    <main>
+                        <Switch>
+                            <Route path="/home" component={HomeScreen} />
+                            <Redirect exact from="/" to="/home" />
+                        </Switch>
+                    </main>
+                </>
+            </Router>
+        </>
+    );
 };
 
 export default App;
