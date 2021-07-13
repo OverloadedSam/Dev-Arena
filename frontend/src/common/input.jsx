0c492e8aeb63f224853a3d78ac91cf7e717374b3
Samdish <samdeeshsingh001@gmail.com>
2021-07-13 09:18:03 +0530

    Add common input component for form

---
+++ frontend/src/common/input.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+
+class Input extends Component {
+    render() {
+        const { label, idAndName, error, hint, ...rest } = this.props;
+
+        return (
+            <>
+                <Col md={6} className="my-2">
+                    <Form.Label
+                        className="font-weight-bold  text-capitalize"
+                        htmlFor={idAndName}
+                    >
+                        {label ? label : ""}
+                    </Form.Label>
+                    <Form.Control id={idAndName} name={idAndName} {...rest} />
+                    {error && (
+                        <Form.Text className="text-danger">{error}</Form.Text>
+                    )}
+                    {!error && hint ? (
+                        <Form.Text className="font-weight-bold font-italic">
+                            {hint}
+                        </Form.Text>
+                    ) : (
+                        ""
+                    )}
+                </Col>
+            </>
+        );
+    }
+}
+
+Input.defaultProps = {
+    placeholder: "",
+    type: "text",
+    hint: null,
+};
+
+export default Input;
