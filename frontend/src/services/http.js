c8be3340051065ee100444990e11c03edc27540a
Samdish <samdeeshsingh001@gmail.com>
2021-08-02 19:56:52 +0530

    Show toast notification on unexpected errors

--- frontend/src/services/http.js
+++ frontend/src/services/http.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 import auth from "./authService";
+import { toast } from "react-toastify";
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 axios.interceptors.response.use(null, (error) => {
+    const expectedError =
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status < 500;
+
+    if (!expectedError) toast.error("An unexpected error occurred!");
+
     return Promise.reject(error);
 });
 
